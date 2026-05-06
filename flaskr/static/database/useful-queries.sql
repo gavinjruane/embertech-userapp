cc872c01c8520f88072960ddd1ee580b0cc52615
Gavin Ruane <gjrswim+github@gmail.com>
2026-05-06 15:03:23 -0700

    [schema.sql, useful-queries.sql] Added one more useful query #19

--- flaskr/static/database/useful-queries.sql
+++ flaskr/static/database/useful-queries.sql
@@ -2,3 +2,8 @@
 -- Should look like this: | <name> | <username> | <role> |
 --                        | Josh   | jgonz      | Admin  |
 SELECT Users.name, Users.username, Roles.title FROM Users INNER JOIN Roles ON Users.role=Roles.id;
+
+
+-- This query will get all users with the 'Standard' role.
+SELECT Users.name, Users.username, Roles.title FROM Users INNER JOIN Roles ON Users.role=Roles.id
+WHERE Roles.title='Standard';
